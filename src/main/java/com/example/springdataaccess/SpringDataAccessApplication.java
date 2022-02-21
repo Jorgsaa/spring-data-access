e7b61d13836f5514033abded377e53f54507a971
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-21 14:05:46 +0100

    FEAT: added temporary example controllers for Thymeleaf and API

--- src/main/java/com/example/springdataaccess/SpringDataAccessApplication.java
+++ src/main/java/com/example/springdataaccess/SpringDataAccessApplication.java
@@ -2,9 +2,36 @@ package com.example.springdataaccess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringDataAccessApplication {
+
+    @RestController
+    @RequestMapping("/api")
+    public class ExampleRestController {
+
+        @GetMapping
+        public String hello()
+        {
+            return "Hello from API";
+        }
+
+    }
+
+    @Controller
+    @RequestMapping("/")
+    public class ExampleController {
+
+        @GetMapping
+        public String index() {
+            return "index";
+        }
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataAccessApplication.class, args);

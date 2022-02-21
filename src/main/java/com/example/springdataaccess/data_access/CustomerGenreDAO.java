78ba90635319ff50d48b2458f55fe6827550807a
JonasBM <jonas.mela@gmail.com>
2022-02-22 00:29:02 +0100

    FEAT: get favorite genres

--- src/main/java/com/example/springdataaccess/data_access/CustomerGenreDAO.java
+++ src/main/java/com/example/springdataaccess/data_access/CustomerGenreDAO.java
@@ -1,4 +1,64 @@
 package com.example.springdataaccess.data_access;
 
+import com.example.springdataaccess.data_access.model.CustomerGenre;
+import com.example.springdataaccess.data_access.util.SqliteConnectionHelper;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerGenreDAO {
+    public List<CustomerGenre> getFavoriteGenres(int customerID) {
+        List<CustomerGenre> favoriteGenres = new ArrayList<>();
+        String sql = """
+    SELECT Genre.GenreId, Genre.Name, count(*) AS 'GenresBought'
+    FROM Customer
+        INNER JOIN Invoice
+            ON Customer.CustomerId = Invoice.CustomerId
+        INNER JOIN InvoiceLine
+            ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+        INNER JOIN Track
+            ON Track.TrackId = InvoiceLine.TrackId
+        INNER JOIN Genre
+            ON Genre.GenreId = Track.GenreId
+    WHERE Customer.CustomerId = ?
+    GROUP BY Genre.GenreId, Genre.Name
+    HAVING GenresBought = (
+        SELECT count(*) AS 'GenresBought'
+        FROM Customer
+            INNER JOIN Invoice
+                ON Customer.CustomerId = Invoice.CustomerId
+            INNER JOIN InvoiceLine
+                ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+            INNER JOIN Track
+                ON Track.TrackId = InvoiceLine.TrackId
+            INNER JOIN Genre
+                ON Genre.GenreId = Track.GenreId
+        WHERE Customer.CustomerId = 57
+        GROUP BY Genre.Name
+        ORDER BY GenresBought desc LIMIT 1);
+""";
+
+        try (Connection conn =
+                     DriverManager.getConnection(
+                             SqliteConnectionHelper.getURL())) {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, customerID);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int genreID = Integer.parseInt(resultSet.getString("GenreId"));
+                String genreName = resultSet.getString("Name");
+
+                favoriteGenres.add(new CustomerGenre(genreID, genreName));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        return favoriteGenres;
+    }
+
 }

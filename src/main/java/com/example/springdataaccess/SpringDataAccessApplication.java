2bb7ad21c639d2779e6724f14afe209b150e92bf
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-21 12:49:19 +0100

    CHORE: initialized Spring project using IntelliJ

---
+++ src/main/java/com/example/springdataaccess/SpringDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAccessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdataaccess/SpringDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

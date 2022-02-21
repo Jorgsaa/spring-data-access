0eb60f597eb7c6b1def23f86622a754492825925
JonasBM <jonas.mela@gmail.com>
2022-02-22 00:16:18 +0100

    CHORE(CustomerGenreDAOTest): create test class

---
+++ src/test/java/com/example/springdataaccess/data_access/CustomerGenreDAOTest.java
@@ -0,0 +1,17 @@
+package com.example.springdataaccess.data_access;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerGenreDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getFavoriteGenres() {
+    }
+}

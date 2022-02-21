f52f2d4c4c3636ee7560c5e6f2f30e634a11c9f2
JonasBM <jonas.mela@gmail.com>
2022-02-22 00:31:40 +0100

    TEST: get favorite genres invalid negative id

--- src/test/java/com/example/springdataaccess/data_access/CustomerGenreDAOTest.java
+++ src/test/java/com/example/springdataaccess/data_access/CustomerGenreDAOTest.java
@@ -1,17 +1,31 @@
 package com.example.springdataaccess.data_access;
 
+import com.example.springdataaccess.data_access.model.Customer;
+import com.example.springdataaccess.data_access.model.CustomerGenre;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerGenreDAOTest {
+    CustomerGenreDAO testCustomerGenreDAO;
 
     @BeforeEach
     void setUp() {
+        testCustomerGenreDAO = new CustomerGenreDAO();
     }
 
     @Test
-    void getFavoriteGenres() {
+    void getFavoriteGenres_InvalidNegativeID_ShouldReturnEmptyList() {
+        List<CustomerGenre> expectedResult = new ArrayList<>();
+        List<CustomerGenre> actualResult;
+        int negativeID = -1;
+
+        actualResult = testCustomerGenreDAO.getFavoriteGenres(negativeID);
+
+        assertEquals(expectedResult, actualResult);
     }
 }

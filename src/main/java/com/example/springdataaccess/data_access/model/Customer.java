4a865767ee5a659d1f863594e5e5b9b9c3f60b86
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-21 16:00:35 +0100

    REFACTOR: changed `Customer` to class and removed some unused fields

--- src/main/java/com/example/springdataaccess/data_access/model/Customer.java
+++ src/main/java/com/example/springdataaccess/data_access/model/Customer.java
@@ -1,17 +1,102 @@
 package com.example.springdataaccess.data_access.model;
 
-public record Customer (
-        Integer id,
-        String firstName,
-        String lastName,
-        String company,
-        String address,
-        String city,
-        String state,
-        String country,
-        String postalCode,
-        String phone,
-        String fax,
-        String email,
-        Integer supportRepId
-) {}
+public class Customer {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    private String phone;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String firstName, String lastName, String company, String address, String city, String state, String country, String postalCode, String phone, String fax, String email, Integer supportRepId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}

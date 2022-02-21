31123a3e02be024305677b424bb13bcd88129a46
JonasBM <jonas.mela@gmail.com>
2022-02-21 22:02:05 +0100

    Chore(CustomerGenre): create test-file

---
+++ src/test/java/com/example/springdataaccess/data_access/model/CustomerGenreTest.java
@@ -0,0 +1,21 @@
+package com.example.springdataaccess.data_access.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerGenreTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCustomerID() {
+    }
+
+    @Test
+    void getFavoriteGenre() {
+    }
+}

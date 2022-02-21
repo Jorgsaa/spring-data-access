9d938879a517a0f59bdd7b6d1ade0480ebfc0994
JonasBM <jonas.mela@gmail.com>
2022-02-21 22:44:51 +0100

    FEAT: add url

--- src/main/java/com/example/springdataaccess/data_access/util/SqliteConnectionHelper.java
+++ src/main/java/com/example/springdataaccess/data_access/util/SqliteConnectionHelper.java
@@ -1,4 +1,7 @@
 package com.example.springdataaccess.data_access.util;
 
 public class SqliteConnectionHelper {
+    public static String getURL() {
+        return "jdbc:sqlite:src/main/resources/Chinook_Sqlite.sqlite";
+    }
 }

63d0b17fa444885ac2d21a946d8408613838b87e
JonasBM <jonas.mela@gmail.com>
2022-02-21 22:00:35 +0100

    FEAT(CustomerGenre): add constructor, getter, setter

--- src/main/java/com/example/springdataaccess/data_access/model/CustomerGenre.java
+++ src/main/java/com/example/springdataaccess/data_access/model/CustomerGenre.java
@@ -1,5 +1,29 @@
 package com.example.springdataaccess.data_access.model;
 
 public class CustomerGenre {
+    private int customerID;
+    private String favoriteGenre;
 
+    CustomerGenre (int customerID, String favoriteGenre) {
+        setCustomerID(customerID);
+        setFavoriteGenre(favoriteGenre);
+    }
+
+    // Getters
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getFavoriteGenre() {
+        return favoriteGenre;
+    }
+
+    // Setters
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setFavoriteGenre(String favoriteGenre) {
+        this.favoriteGenre = favoriteGenre;
+    }
 }

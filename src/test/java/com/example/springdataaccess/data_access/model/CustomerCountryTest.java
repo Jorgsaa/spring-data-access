eef4967912bfe28a41581f8a980f70c0ebd5fa08
JonasBM <jonas.mela@gmail.com>
2022-02-22 14:55:48 +0100

    TEST: add setup

--- src/test/java/com/example/springdataaccess/data_access/model/CustomerCountryTest.java
+++ src/test/java/com/example/springdataaccess/data_access/model/CustomerCountryTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerCountryTest {
+    CustomerCountry testCustomerCountry;
+    String originalCountryName = "Russia";
+    int originalNumberOfCustomers = 10;
 
     @BeforeEach
     void setUp() {
+        testCustomerCountry = new CustomerCountry(originalCountryName, originalNumberOfCustomers);
     }
 
     @Test

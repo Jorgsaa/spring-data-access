3f82c9a96a6d92c5fc15fda6a25d5f107cbb0ba6
Jonas Kristoffersen <jo_nas@live.no>
2022-02-22 21:33:01 +0100

    fix: fixed static resources in the web application not being loaded, resulting in 404 error
    Removed EnableWebMvc annotation at application entrypoint (SpringDataAccessApplication).
    
    Added line in application.properties to specify path matching-strategy as 'ANT_PATH_MATCHER'.

--- src/main/java/com/example/springdataaccess/SpringDataAccessApplication.java
+++ src/main/java/com/example/springdataaccess/SpringDataAccessApplication.java
@@ -2,10 +2,8 @@ package com.example.springdataaccess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SpringDataAccessApplication {
 
     public static void main(String[] args) {

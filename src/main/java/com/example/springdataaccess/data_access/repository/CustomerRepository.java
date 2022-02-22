69764f59af1d05cdabb0c040b7eda18113ea21fc
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-22 14:58:00 +0100

    FEAT: implemented `findById` and `findByName` methods in repository implementation

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import com.example.springdataaccess.data_access.model.Customer;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository {
 

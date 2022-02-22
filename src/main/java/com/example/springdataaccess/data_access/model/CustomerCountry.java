17559492c86d2410cb28668a44a894d8fb20fbc8
JonasBM <jonas.mela@gmail.com>
2022-02-22 14:53:16 +0100

    FIX: make constructor public

--- src/main/java/com/example/springdataaccess/data_access/model/CustomerCountry.java
+++ src/main/java/com/example/springdataaccess/data_access/model/CustomerCountry.java
@@ -4,7 +4,7 @@ public class CustomerCountry {
     private String countryName;
     private int numberOfCustomers;
 
-    CustomerCountry(String countryName, int numberOfCustomers) {
+    public CustomerCountry(String countryName, int numberOfCustomers) {
         setCountryName(countryName);
         setNumberOfCustomers(numberOfCustomers);
     }

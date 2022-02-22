3c04431f49285191708551262627c81a5b82a359
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-22 16:26:57 +0100

    REFACTOR: rename `ViewController` -> `IndexController`

--- src/main/java/com/example/springdataaccess/controller/view/IndexController.java
+++ src/main/java/com/example/springdataaccess/controller/view/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class ViewController {
+public class IndexController {
 
     @GetMapping
     public String index() {

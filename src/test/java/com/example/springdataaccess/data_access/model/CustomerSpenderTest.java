e3cedfa54c155cfded0cfdfa5dd0e1cda1b02b3f
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:37:30 +0100

    TEST: add setup

--- src/test/java/com/example/springdataaccess/data_access/model/CustomerSpenderTest.java
+++ src/test/java/com/example/springdataaccess/data_access/model/CustomerSpenderTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerSpenderTest {
+    CustomerSpender testCustomerSpender;
+    int originalCustomerId = 1;
+    int originalHighestSpending = 1000000;
 
     @BeforeEach
     void setUp() {
+        testCustomerSpender = new CustomerSpender(originalCustomerId, originalHighestSpending);
     }
 
     @Test

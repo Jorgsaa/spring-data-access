eaaaa73eca0121e4fb3a7fb2129206979189234c
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:53:19 +0100

    DOCS: method purpose

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class CustomerSpenderRepository {
 
     public ArrayList<CustomerSpender> getHighestSpenders() {
+        // Return the highest spending of each customer (descending/highest order first)
         ArrayList<CustomerSpender> highestSpenders = new ArrayList<>();
 
         return highestSpenders;

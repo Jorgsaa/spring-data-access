922a8ba50e01968f6f364da0fb16de914b1bc618
JonasBM <jonas.mela@gmail.com>
2022-02-22 16:17:33 +0100

    DOCS: comment

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class CustomerSpenderRepository {
 
     public ArrayList<CustomerSpender> getHighestSpenders() {
-        // Return the highest spending of each customer (descending/highest order first)
+        // Return the highest spending of each customer (descending/highest invoice first)
         ArrayList<CustomerSpender> highestSpenders = new ArrayList<>();
         String sql = """
                 SELECT Customer.CustomerId, max(Total) AS HighestSpending

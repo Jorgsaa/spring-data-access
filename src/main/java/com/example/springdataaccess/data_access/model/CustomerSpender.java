feb191b34c7d08bf69c919e9aa4aa2ea46f01581
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:33:03 +0100

    FEAT: add getters

--- src/main/java/com/example/springdataaccess/data_access/model/CustomerSpender.java
+++ src/main/java/com/example/springdataaccess/data_access/model/CustomerSpender.java
@@ -4,4 +4,12 @@ public class CustomerSpender {
     private int customerId;
     private int highestSpending;
 
+    // Getters
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getHighestSpending() {
+        return highestSpending;
+    }
 }

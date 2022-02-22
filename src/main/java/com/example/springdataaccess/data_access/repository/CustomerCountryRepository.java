aae586b31ab262202459a35dd195f4b21f383693
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:08:57 +0100

    CHORE: create method for getting customers in each country

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerCountryRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerCountryRepository.java
@@ -1,4 +1,13 @@
 package com.example.springdataaccess.data_access.repository;
 
+import com.example.springdataaccess.data_access.model.CustomerCountry;
+
+import java.util.ArrayList;
+
 public class CustomerCountryRepository {
+    public ArrayList<CustomerCountry> getCustomersInEachCountry() {
+        ArrayList<CustomerCountry> customersInEachCountry = new ArrayList<>();
+
+        return customersInEachCountry;
+    }
 }

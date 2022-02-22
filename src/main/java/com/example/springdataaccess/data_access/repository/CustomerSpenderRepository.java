943ea7b8330b6dd8b2410a92c08083d0dae6d36e
JonasBM <jonas.mela@gmail.com>
2022-02-22 16:15:31 +0100

    FEAT: get highest spenders

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
@@ -1,7 +1,10 @@
 package com.example.springdataaccess.data_access.repository;
 
+import com.example.springdataaccess.data_access.model.CustomerGenre;
 import com.example.springdataaccess.data_access.model.CustomerSpender;
+import com.example.springdataaccess.data_access.util.SqliteConnectionHelper;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CustomerSpenderRepository {
@@ -9,6 +12,32 @@ public class CustomerSpenderRepository {
     public ArrayList<CustomerSpender> getHighestSpenders() {
         // Return the highest spending of each customer (descending/highest order first)
         ArrayList<CustomerSpender> highestSpenders = new ArrayList<>();
+        String sql = """
+                SELECT Customer.CustomerId, max(Total) AS HighestSpending
+                FROM Customer
+                    INNER JOIN Invoice
+                        ON Customer.CustomerId = Invoice.CustomerId
+                GROUP BY Customer.CustomerId, FirstName
+                ORDER BY HighestSpending desc;
+                """;
+
+        try (Connection conn =
+                     DriverManager.getConnection(
+                             SqliteConnectionHelper.getURL())) {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int customerId = Integer.parseInt(resultSet.getString("CustomerId"));
+                int highestSpending = Integer.parseInt(resultSet.getString("HighestSpending"));
+
+                highestSpenders.add(new CustomerSpender(customerId, highestSpending));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
         return highestSpenders;
     }

7a16eb316fd29d454c563dab49c808ac1b6ca395
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:49:27 +0100

    CHORE: create method for getting the highest spenders

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerSpenderRepository.java
@@ -1,4 +1,14 @@
 package com.example.springdataaccess.data_access.repository;
 
+import com.example.springdataaccess.data_access.model.CustomerSpender;
+
+import java.util.ArrayList;
+
 public class CustomerSpenderRepository {
+
+    public ArrayList<CustomerSpender> getHighestSpenders() {
+        ArrayList<CustomerSpender> highestSpenders = new ArrayList<>();
+
+        return highestSpenders;
+    }
 }

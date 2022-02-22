1594315f27b2efd6c507e6bd088d5ba17f46bc2b
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:11:21 +0100

    DOCS: comment purpose

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerCountryRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerCountryRepository.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public class CustomerCountryRepository {
     public ArrayList<CustomerCountry> getCustomersInEachCountry() {
+        // Return a list of all countries with their corresponding number of customers
+        // (descending by number of customers in each country)
         ArrayList<CustomerCountry> customersInEachCountry = new ArrayList<>();
 
         return customersInEachCountry;

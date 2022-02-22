cb36fbd9b38033a67c887847a64bc88fb92a9f96
JonasBM <jonas.mela@gmail.com>
2022-02-22 15:35:50 +0100

    CHORE: create test class

---
+++ src/test/java/com/example/springdataaccess/data_access/model/CustomerSpenderTest.java
@@ -0,0 +1,21 @@
+package com.example.springdataaccess.data_access.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerSpenderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCustomerId() {
+    }
+
+    @Test
+    void getHighestSpending() {
+    }
+}

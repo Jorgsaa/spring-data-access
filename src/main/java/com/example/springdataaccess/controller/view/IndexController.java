9f5ff521de1f624a416820b021b1841929835e79
Jonas Kristoffersen <jo_nas@live.no>
2022-02-23 16:03:38 +0100

    feat: implemented list fragment used on home page as well as fragments representing list items

--- src/main/java/com/example/springdataaccess/controller/view/IndexController.java
+++ src/main/java/com/example/springdataaccess/controller/view/IndexController.java
@@ -1,15 +1,73 @@
 package com.example.springdataaccess.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    public class Artist {
+        public String name;
+
+        public Artist(String name) {
+            this.name = name;
+        }
+    }
+
+    public class SongAlbum {
+        public String songTitle;
+        public String albumTitle;
+
+        public SongAlbum(String songTitle, String albumTitle) {
+            this.songTitle = songTitle;
+            this.albumTitle = albumTitle;
+        }
+    }
+
+    public class Genre {
+        public String genre;
+
+        public Genre(String genre) {
+            this.genre = genre;
+        }
+    }
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        // Temporary data
+        Collection<Artist> artists = List.of(
+            new Artist("Drake"),
+            new Artist("Clams Casino"),
+            new Artist("Daft Punk"),
+            new Artist("Jimmy neutron"),
+            new Artist("Michael Jackson")
+        );
+
+        Collection<SongAlbum> songAlbums = List.of(
+                new SongAlbum("Songtitle1", "Albumtitle1"),
+                new SongAlbum("Songtitle2", "Albumtitle2"),
+                new SongAlbum("Songtitle3", "Albumtitle3"),
+                new SongAlbum("Songtitle4", "Albumtitle4"),
+                new SongAlbum("Songtitle5", "Albumtitle5")
+        );
+
+        Collection<Genre> genres = List.of(
+                new Genre("genre1"),
+                new Genre("genre2"),
+                new Genre("genre3"),
+                new Genre("genre4"),
+                new Genre("genre5")
+        );
+
+        model.addAttribute("artists", artists);
+        model.addAttribute("songAlbums", songAlbums);
+        model.addAttribute("genres", genres);
+
         return "index";
     }
 

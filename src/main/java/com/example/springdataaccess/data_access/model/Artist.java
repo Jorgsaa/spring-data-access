e5a4d346e091ec4e8ca69089d5fcc2d4a5710591
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-23 15:34:30 +0100

    FEAT: added constructors, getters, and setters to `Artist`, `Genre`, and `Track`

--- src/main/java/com/example/springdataaccess/data_access/model/Artist.java
+++ src/main/java/com/example/springdataaccess/data_access/model/Artist.java
@@ -3,4 +3,29 @@ package com.example.springdataaccess.data_access.model;
 public class Artist {
     private int artistId;
     private String name;
+
+    public Artist() {
+    }
+
+    public Artist(int artistId, String name) {
+        this.artistId = artistId;
+        this.name = name;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }

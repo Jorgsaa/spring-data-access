a5672a2171f9bb9087d31305c4b360234ec7114b
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-23 15:58:50 +0100

    FEAT: implemented `MusicService`

---
+++ src/main/java/com/example/springdataaccess/service/MusicServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.springdataaccess.service;
+
+import com.example.springdataaccess.data_access.repository.ArtistRepository;
+import com.example.springdataaccess.data_access.repository.GenreRepository;
+import com.example.springdataaccess.data_access.repository.TrackRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class MusicServiceImpl implements MusicService {
+
+    private final ArtistRepository artists;
+    private final GenreRepository genres;
+    private final TrackRepository tracks;
+
+    public MusicServiceImpl(ArtistRepository artists, GenreRepository genres, TrackRepository tracks) {
+        this.artists = artists;
+        this.genres = genres;
+        this.tracks = tracks;
+    }
+
+    @Override
+    public HashMap<String, List<?>> fetchMusicData(int amountEach) {
+        HashMap<String, List<?>> musicData = new HashMap<>();
+        musicData.put("artists", artists.findRandom(amountEach));
+        musicData.put("genres", genres.findRandom(amountEach));
+        musicData.put("tracks", tracks.findRandom(amountEach));
+        return musicData;
+    }
+
+}

f9d722fdd4a1695937bd61700e583153b6b9673a
JonasBM <jonas.mela@gmail.com>
2022-02-24 12:15:42 +0100

    FEAT: add method

--- src/main/java/com/example/springdataaccess/data_access/repository/CustomerCountryRepository.java
+++ src/main/java/com/example/springdataaccess/data_access/repository/CustomerCountryRepository.java
@@ -1,4 +1,10 @@
 package com.example.springdataaccess.data_access.repository;
 
+import com.example.springdataaccess.data_access.model.Customer;
+
+import java.util.Collection;
+import java.util.List;
+
 public interface CustomerCountryRepository {
+    List<Customer> findAll();
 }

993914a90587f8cf2ebb7f9460af8d9fadd3879a
JonasBM <jonas.mela@gmail.com>
2022-02-24 15:48:07 +0100

    FEAT: find the highest spenders

--- src/main/java/com/example/springdataaccess/controller/api/CustomerSpenderController.java
+++ src/main/java/com/example/springdataaccess/controller/api/CustomerSpenderController.java
@@ -1,8 +1,13 @@
 package com.example.springdataaccess.controller.api;
 
+import com.example.springdataaccess.data_access.model.CustomerSpender;
 import com.example.springdataaccess.data_access.repository.CustomerSpenderRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api")
@@ -11,5 +16,11 @@ public class CustomerSpenderController {
 
     public CustomerSpenderController(CustomerSpenderRepository customerSpenders) {
         this.customerSpenders = customerSpenders;
+    }
+
+    @GetMapping("/spender")
+    @Operation(summary = "Get the highest spenders")
+    public ArrayList<CustomerSpender> findAll() {
+        return customerSpenders.getHighestSpenders();
     }
 }

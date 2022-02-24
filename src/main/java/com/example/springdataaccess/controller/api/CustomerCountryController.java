e838dfd13cd33212a8a9372230ac1d6a9fa473d9
JonasBM <jonas.mela@gmail.com>
2022-02-24 12:24:54 +0100

    FEAT: add class endpoint

--- src/main/java/com/example/springdataaccess/controller/api/CustomerCountryController.java
+++ src/main/java/com/example/springdataaccess/controller/api/CustomerCountryController.java
@@ -1,4 +1,14 @@
 package com.example.springdataaccess.controller.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Customer Country")
+@RequestMapping("/api")
 public class CustomerCountryController {
+
+
 }

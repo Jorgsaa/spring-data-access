7b4a4883d52365aad9c0518ad322f1cf0e7e5fcb
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-24 10:46:48 +0100

    FEAT: add `searchResults` as an attribute in `/search`

--- src/main/java/com/example/springdataaccess/controller/view/SearchController.java
+++ src/main/java/com/example/springdataaccess/controller/view/SearchController.java
@@ -1,9 +1,9 @@
 package com.example.springdataaccess.controller.view;
 
+import com.example.springdataaccess.service.MusicService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,11 +11,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/search")
 public class SearchController {
 
+    private final MusicService musicService;
+
+    public SearchController(MusicService musicService) {
+        this.musicService = musicService;
+    }
+
     @GetMapping
     public String search(Model model, @RequestParam(value = "term", defaultValue = "") String searchTerm) {
-        if (searchTerm.isBlank())
-            return "redirect:/";
+        // Redirect to home page if search term is blank
+        if (searchTerm.isBlank()) return "redirect:/";
+        // Add the search term as an attribute
         model.addAttribute("searchTerm", searchTerm);
+        // Add the search results as an attribute
+        model.addAttribute("searchResults", musicService.searchMusicData(searchTerm));
         return "search";
     }
 
